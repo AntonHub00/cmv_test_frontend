38263e8f6ebc1193205719a744a7ab7baa908286
Antonio Ochoa <antonhub00@gmail.com>
2021-07-13 21:43:58 -0500

    Axios and Vue Axios added

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
+const axiosInstance = axios.create({
+	baseURL: process.env.VUE_APP_API_URL,
+})
+
+Vue.use(VueAxios, axiosInstance)
 
 new Vue({
 	router,
