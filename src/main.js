2cd402ca33a0893f0ab775275d8627d08c247f44
Antonio Ochoa <antonhub00@gmail.com>
2021-07-13 23:02:30 -0500

    BootstrapVue added to project

--- src/main.js
+++ src/main.js
@@ -4,12 +4,21 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const axiosInstance = axios.create({
 	baseURL: process.env.VUE_APP_API_URL,
 })
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue) // Make BootstrapVue available throughout the project
+
+Vue.use(IconsPlugin) // Optionally install the BootstrapVue icon components plugin
 
 Vue.use(VueAxios, axiosInstance)
 
