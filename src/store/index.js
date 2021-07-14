4b41e34907342d2dc18996418884d75935e94f08
Antonio Ochoa <antonhub00@gmail.com>
2021-07-13 21:47:38 -0500

    Vuex module added

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@/store/modules/api.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
-	modules: {},
+	modules: { api },
 })
